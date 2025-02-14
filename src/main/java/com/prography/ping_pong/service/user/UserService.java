a70132ff21179be78ea791a78610e054196621af
coli <kkwoo001021@naver.com>
2025-02-15 01:01:11 +0900

    feat: 페이징된 유저 조회 서비스 로직 구현

--- src/main/java/com/prography/ping_pong/service/user/UserService.java
+++ src/main/java/com/prography/ping_pong/service/user/UserService.java
@@ -6,6 +6,7 @@ import com.prography.ping_pong.domain.user.User;
 import com.prography.ping_pong.dto.request.user.FakerRequest;
 import com.prography.ping_pong.dto.request.user.UserInitializeRequest;
 import com.prography.ping_pong.dto.response.ApiResponse;
+import com.prography.ping_pong.dto.response.user.UserPageResponse;
 import com.prography.ping_pong.repository.RoomRepository;
 import com.prography.ping_pong.repository.UserRepository;
 import com.prography.ping_pong.repository.UserRoomRepository;
@@ -14,6 +15,8 @@ import com.prography.ping_pong.service.user.client.FakerUserDetails;
 import com.prography.ping_pong.service.user.client.UserInitializeClient;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -53,5 +56,10 @@ public class UserService {
         userRoomRepository.deleteAllWithFlush(userRooms);
         roomRepository.deleteAllWithFlush(rooms);
         userRepository.deleteAllWithFlush(users);
+    }
+
+    public UserPageResponse findAll(Pageable pageable) {
+        Page<User> foundUsers = userRepository.findAllByOrderByIdAsc(pageable);
+        return new UserPageResponse(foundUsers);
     }
 }
---
+++ src/test/java/com/prography/ping_pong/service/user/UserServiceTest.java
@@ -0,0 +1,41 @@
+package com.prography.ping_pong.service.user;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.prography.ping_pong.common.BaseServiceTest;
+import com.prography.ping_pong.domain.user.User;
+import com.prography.ping_pong.domain.user.UserStatus;
+import com.prography.ping_pong.dto.response.user.UserPageResponse;
+import com.prography.ping_pong.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+
+class UserServiceTest extends BaseServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("페이징된 유저 목록을 조회한다")
+    @Test
+    void findAllUsers() {
+        User user1 = new User(1L, "name1", "email1@email.com", UserStatus.ACTIVE);
+        User user2 = new User(2L, "name2", "email2@email.com", UserStatus.ACTIVE);
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        UserPageResponse response = userService.findAll(Pageable.ofSize(1).withPage(1));
+
+        assertAll(
+                () -> assertThat(response.totalPages()).isEqualTo(2L),
+                () -> assertThat(response.totalElements()).isEqualTo(2L),
+                () -> assertThat(response.userList()).hasSize(1),
+                () -> assertThat(response.userList().get(0).id()).isEqualTo(2L)
+        );
+    }
+}
