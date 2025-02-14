09f32cbf9d4dce0decb533a3d5c55d20fca7402c
coli <kkwoo001021@naver.com>
2025-02-14 15:45:56 +0900

    feat: RestClient 설정 등록

---
+++ src/main/java/com/prography/ping_pong/config/PrographyProperties.java
@@ -0,0 +1,8 @@
+package com.prography.ping_pong.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "prography")
+public record PrographyProperties(String baseUrl) {
+
+}
---
+++ src/main/java/com/prography/ping_pong/config/UserClientConfig.java
@@ -0,0 +1,28 @@
+package com.prography.ping_pong.config;
+
+import java.time.Duration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class UserClientConfig {
+
+    private static final Duration DEFAULT_CONNECTION_TIMEOUT = Duration.ofSeconds(60);
+    private static final Duration DEFAULT_READ_TIMEOUT = Duration.ofSeconds(30);
+
+    @Bean
+    public RestClient.Builder restClientBuilder() {
+        return RestClient.builder()
+                .requestFactory(clientHttpRequestFactory());
+    }
+
+    private ClientHttpRequestFactory clientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(DEFAULT_CONNECTION_TIMEOUT);
+        requestFactory.setReadTimeout(DEFAULT_READ_TIMEOUT);
+        return requestFactory;
+    }
+}
