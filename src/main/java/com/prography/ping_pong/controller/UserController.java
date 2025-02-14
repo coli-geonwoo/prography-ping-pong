fd57d576a60027247ed8676e80ff1bd8758f7228
coli <kkwoo001021@naver.com>
2025-02-15 01:16:00 +0900

    feat: 유저 전체 조회 api 구현

--- src/main/java/com/prography/ping_pong/controller/UserController.java
+++ src/main/java/com/prography/ping_pong/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.prography.ping_pong.controller;
 
 import com.prography.ping_pong.dto.request.user.UserInitializeRequest;
+import com.prography.ping_pong.dto.response.ApiBodyResponse;
 import com.prography.ping_pong.dto.response.ApiResponse;
+import com.prography.ping_pong.dto.response.user.UserPageResponse;
 import com.prography.ping_pong.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,6 +24,17 @@ public class UserController {
     @PostMapping("/init")
     public ResponseEntity<ApiResponse> initialize(@RequestBody UserInitializeRequest request) {
         ApiResponse response = userService.initialize(request);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<ApiBodyResponse<UserPageResponse>> findUsers(
+            @RequestParam(name = "size") int size,
+            @RequestParam(name = "page") int page
+    ) {
+        Pageable pageable = PageRequest.of(page, size);
+        UserPageResponse pageResponse = userService.findAll(pageable);
+        ApiBodyResponse<UserPageResponse> response = ApiBodyResponse.ok(pageResponse);
         return ResponseEntity.ok(response);
     }
 }
