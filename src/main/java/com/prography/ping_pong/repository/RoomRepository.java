6718cde0354d55a2d1355ca47cb01b5ff1e7882f
coli <kkwoo001021@naver.com>
2025-02-15 02:10:40 +0900

    feat: 방 전체 조회 API 구현

--- src/main/java/com/prography/ping_pong/repository/RoomRepository.java
+++ src/main/java/com/prography/ping_pong/repository/RoomRepository.java
@@ -1,13 +1,19 @@
 package com.prography.ping_pong.repository;
 
 import com.prography.ping_pong.domain.room.Room;
+import com.prography.ping_pong.domain.user.User;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface RoomRepository extends JpaRepository<Room, Long> {
+
+
+    Page<Room> findAllByOrderByIdAsc(Pageable pageable);
 
     @Query("DELETE FROM Room room WHERE room IN :rooms")
     @Modifying(clearAutomatically = true, flushAutomatically = true)
--- src/main/java/com/prography/ping_pong/service/room/RoomService.java
+++ src/main/java/com/prography/ping_pong/service/room/RoomService.java
@@ -2,10 +2,13 @@ package com.prography.ping_pong.service.room;
 
 import com.prography.ping_pong.domain.room.Room;
 import com.prography.ping_pong.dto.response.room.RoomDetailResponse;
+import com.prography.ping_pong.dto.response.room.RoomPageResponse;
 import com.prography.ping_pong.exception.custom.PingPongClientErrorException;
 import com.prography.ping_pong.exception.errorcode.ClientErrorCode;
 import com.prography.ping_pong.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,6 +16,11 @@ import org.springframework.stereotype.Service;
 public class RoomService {
 
     private final RoomRepository roomRepository;
+
+    public RoomPageResponse findAll(Pageable pageable) {
+        Page<Room> roomPage = roomRepository.findAll(pageable);
+        return new RoomPageResponse(roomPage);
+    }
 
     public RoomDetailResponse findRoom(long roomId) {
         Room room = findById(roomId);
