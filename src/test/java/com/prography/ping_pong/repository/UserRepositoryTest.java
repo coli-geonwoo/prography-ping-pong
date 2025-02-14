2a5d8162fb346ca455f38da3fd778af6e8efdd5d
coli <kkwoo001021@naver.com>
2025-02-15 01:00:54 +0900

    feat: 페이징된 유저 조회 메서드 구현

---
+++ src/test/java/com/prography/ping_pong/repository/UserRepositoryTest.java
@@ -0,0 +1,51 @@
+package com.prography.ping_pong.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import com.prography.ping_pong.common.BaseRepositoryTest;
+import com.prography.ping_pong.domain.user.User;
+import com.prography.ping_pong.domain.user.UserStatus;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+
+class UserRepositoryTest extends BaseRepositoryTest {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @DisplayName("모든 유저를 배치 쿼리로 삭제한다")
+    @Test
+    void deleteAllWithFlush() {
+        User user1 = new User(1L, "name1", "email1@email.com", UserStatus.ACTIVE);
+        User user2 = new User(2L, "name2", "email2@email.com", UserStatus.ACTIVE);
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        userRepository.deleteAllWithFlush(Arrays.asList(user1, user2));
+
+        long count = userRepository.count();
+        assertThat(count).isZero();
+    }
+
+    @DisplayName("페이징된 멤버를 조회한다")
+    @Test
+    void findAllByOrderByIdAsc() {
+        PageRequest pageRequest = PageRequest.of(1, 1);
+        User user1 = new User(1L, "name1", "email1@email.com", UserStatus.ACTIVE);
+        User user2 = new User(2L, "name2", "email2@email.com", UserStatus.ACTIVE);
+        userRepository.save(user1);
+        userRepository.save(user2);
+
+        Page<User> users = userRepository.findAllByOrderByIdAsc(pageRequest);
+
+        assertAll(
+                () -> assertThat(users.getContent()).hasSize(1),
+                () -> assertThat(users.getContent().get(0).getId()).isEqualTo(2L)
+        );
+    }
+}
