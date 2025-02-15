4f4a1aed25cf4471e94e9491eb2c326214ebfb76
coli <kkwoo001021@naver.com>
2025-02-16 01:13:36 +0900

    test: UserServiceTest 추가

--- src/test/java/com/prography/ping_pong/service/userroom/UserRoomServiceTest.java
+++ src/test/java/com/prography/ping_pong/service/userroom/UserRoomServiceTest.java
@@ -1,18 +1,24 @@
 package com.prography.ping_pong.service.userroom;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.prography.ping_pong.common.BaseServiceTest;
 import com.prography.ping_pong.domain.room.Room;
+import com.prography.ping_pong.domain.room.RoomStatus;
 import com.prography.ping_pong.domain.room.RoomType;
 import com.prography.ping_pong.domain.user.User;
 import com.prography.ping_pong.domain.user.UserStatus;
 import com.prography.ping_pong.domain.userroom.Team;
 import com.prography.ping_pong.domain.userroom.UserRoom;
+import com.prography.ping_pong.exception.custom.PingPongClientErrorException;
+import com.prography.ping_pong.view.ResponseMessage;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.EnumSource.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 
 class UserRoomServiceTest extends BaseServiceTest {
@@ -62,5 +68,63 @@ class UserRoomServiceTest extends BaseServiceTest {
 
         List<UserRoom> allRoomUsers = userRoomRepository.findAllByRoomId(savedRoom.getId());
         assertThat(allRoomUsers).hasSize(1);
+    }
+
+    @DisplayName("특정 유저의 팀을 변경시킨다")
+    @Test
+    void changeTeam() {
+        User user1 = new User(1L, "name1", "email1@email.com", UserStatus.ACTIVE);
+        User savedUser1 = userRepository.save(user1);
+
+        Room dummy = new Room("room1", savedUser1, RoomType.SINGLE);
+        Room savedRoom = roomRepository.save(dummy);
+
+        Team team = Team.RED;
+        UserRoom userRoom1 = new UserRoom(savedUser1, dummy, team);
+        UserRoom savedUserRoom1 = userRoomRepository.save(userRoom1);
+
+        userRoomService.changeTeam(savedUser1.getId(), savedRoom.getId());
+
+        UserRoom userRoom = userRoomRepository.findByUserIdAndRoomId(savedUser1.getId(), savedRoom.getId()).get();
+        assertThat(userRoom.getTeam()).isEqualTo(team.opposite());
+    }
+
+    @DisplayName("이미 방이 시작되었다면 팀을 변경할 수 없다")
+    @ParameterizedTest
+    @EnumSource(value = RoomStatus.class, mode = Mode.EXCLUDE, names = "WAIT")
+    void canNotChangeTeamWhenRoomStatusIsNotWait(RoomStatus notWaitStatus) {
+        User user1 = new User(1L, "name1", "email1@email.com", UserStatus.ACTIVE);
+        User savedUser1 = userRepository.save(user1);
+
+        Room dummy = new Room(null, "room1", savedUser1, RoomType.SINGLE, notWaitStatus);
+        Room savedRoom = roomRepository.save(dummy);
+
+        UserRoom userRoom1 = new UserRoom(savedUser1, dummy, Team.RED);
+        UserRoom savedUserRoom1 = userRoomRepository.save(userRoom1);
+
+        assertThatThrownBy(() -> userRoomService.changeTeam(savedUser1.getId(), savedRoom.getId()))
+                .isInstanceOf(PingPongClientErrorException.class)
+                .hasMessage(ResponseMessage.CLIENT_ERROR.getValue());
+    }
+
+    @DisplayName("변경하려는 팀 인원이 모두 차있다면 팀을 변경할 수 없다")
+    @Test
+    void canNotChangeTeamWhenOppositeTeamIsFull() {
+        User user1 = new User(1L, "name1", "email1@email.com", UserStatus.ACTIVE);
+        User user2 = new User(2L, "name2", "email2@email.com", UserStatus.ACTIVE);
+        User savedUser1 = userRepository.save(user1);
+        User savedUser2 = userRepository.save(user2);
+
+        Room dummy = new Room("room1", savedUser1, RoomType.SINGLE);
+        Room savedRoom = roomRepository.save(dummy);
+
+        UserRoom userRoom1 = new UserRoom(savedUser1, dummy, Team.RED);
+        UserRoom userRoom2 = new UserRoom(savedUser2, dummy, Team.BLUE);
+        UserRoom savedUserRoom1 = userRoomRepository.save(userRoom1);
+        UserRoom savedUserRoom2 = userRoomRepository.save(userRoom2);
+
+        assertThatThrownBy(() -> userRoomService.changeTeam(savedUser1.getId(), savedRoom.getId()))
+                .isInstanceOf(PingPongClientErrorException.class)
+                .hasMessage(ResponseMessage.CLIENT_ERROR.getValue());
     }
 }
