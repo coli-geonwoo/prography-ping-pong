52526b04dadeddcab006c37a25f006834a70ceb6
coli <kkwoo001021@naver.com>
2025-02-17 20:31:14 +0900

    chore: schema 정보 입력

--- src/main/java/com/prography/pingpong/dto/response/room/RoomAttendResponse.java
+++ src/main/java/com/prography/pingpong/dto/response/room/RoomAttendResponse.java
@@ -1,7 +1,13 @@
 package com.prography.pingpong.dto.response.room;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record RoomAttendResponse(
+
+        @Schema(description = "방에 참여한 유저 id", example = "1")
         long userId,
+
+        @Schema(description = "참여한 방 id", example = "1")
         long roomId
 ) {
 
--- src/main/java/com/prography/pingpong/dto/response/room/RoomCreateResponse.java
+++ src/main/java/com/prography/pingpong/dto/response/room/RoomCreateResponse.java
@@ -3,15 +3,28 @@ package com.prography.pingpong.dto.response.room;
 import com.prography.pingpong.domain.room.Room;
 import com.prography.pingpong.domain.room.RoomStatus;
 import com.prography.pingpong.domain.room.RoomType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record RoomCreateResponse(
+        @Schema(description = "방 아이디", example = "1")
         long id,
-        @NotBlank String title,
+
+        @NotBlank
+        @Schema(description = "방 제목", example = "단식하실 분")
+        String title,
+
+        @Schema(description = "방장 아이디", example = "1")
         long userId,
-        @NotNull RoomType roomType,
-        @NotNull RoomStatus status
+
+        @NotNull
+        @Schema(description = "방의 타입(단식-복식)", example = "SINGLE")
+        RoomType roomType,
+
+        @NotNull
+        @Schema(description = "방의 상태", example = "WAIT")
+        RoomStatus status
 ) {
 
     public RoomCreateResponse(Room room) {
