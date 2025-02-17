1e834a0e9f1624c8159e7b0b8d8ec7d11d93c7d4
coli <kkwoo001021@naver.com>
2025-02-17 19:46:39 +0900

    fix: swagger 충돌 예방을 위한 hidden 애너테이션 추가

--- src/main/java/com/prography/pingpong/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/prography/pingpong/exception/handler/GlobalExceptionHandler.java
@@ -6,13 +6,17 @@ import com.prography.pingpong.exception.custom.PingPongServerException;
 import com.prography.pingpong.exception.errorcode.ErrorCode;
 import com.prography.pingpong.exception.errorcode.ServerErrorCode;
 import com.prography.pingpong.view.ResponseMessage;
+import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
+@Hidden
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
